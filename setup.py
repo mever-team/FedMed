a86f173a36557b5e69a3718b1aac976a0777f2e4
manios krasanakis <maniospas@hotmail.com>
2024-01-25 04:14:23 +0200

    token-based pypi upload

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ import setuptools
 # Developer self-reminder for uploading in pypi:
 # - install: wheel, twine
 # - build  : python setup.py bdist_wheel
-# - deploy : twine upload dist/*
+# - deploy : twine upload --repository fedmed dist/*
+# https://kynan.github.io/blog/2020/05/23/how-to-upload-your-package-to-the-python-package-index-pypi-test-server
+
 
 with open("README.md", "r") as file:
     long_description = file.read()
@@ -13,10 +15,10 @@ with open("requirements.txt", "r") as file:
 
 setuptools.setup(
     name='FedMed',
-    version='0.2.0',
+    version='0.3.0',
     author="Emmanouil (Manios) Krasanakis",
     author_email="maniospas@hotmail.com",
-    description="A privacy-aware federated computing scheme to let "
+    description="A privacy-aware federated computing library to let "
                 "non-trusted clients perform statistical analysis.",
     long_description=long_description,
     long_description_content_type="text/markdown",
