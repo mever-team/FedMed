03facbd351d20854125de1568902519f82945ee6
maniospas <maniospas@hotmail.com>
2024-03-15 13:10:36 +0200

    added pearson correlation in base operations available

--- example/simulation.py
+++ example/simulation.py
@@ -11,7 +11,7 @@ data = fm.FedData([
     fm.Simulation(server=server, fragment="treatmentA"),
 ])
 
-from fedmed.stats.base import std, mean
+from fedmed.stats.base import pearson
 d1 = data["Gender"] == "Man"
 d2 = data["Receptive"]
-print("Pearson correlation", min(1, mean(d1*d2)-mean(d1)*mean(d2))/(std(d1)*std(d2)))
+print("Pearson correlation", pearson(d1, d2))
--- fedmed/stats/base.py
+++ fedmed/stats/base.py
@@ -1,6 +1,7 @@
 from fedmed.core import FedData, RemoteRunnable
 
-
+_min = min
+_max = max
 sum = RemoteRunnable("sum")
 len = RemoteRunnable("len")
 max = RemoteRunnable("max")
@@ -22,3 +23,8 @@ def var(data: FedData, df: int = 0):
 
 def std(data: FedData, df: int = 0):
     return var(data, df)**0.5
+
+
+def pearson(d1: FedData, d2: FedData):
+    return _max(-1, _min(1, mean(d1 * d2) - mean(d1) * mean(d2)) / (std(d1) * std(d2)))
+
